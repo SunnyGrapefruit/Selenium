28489c3879a0d1bcf702e672fc8f0b7e764c851b
SunnyGrapefruit <wir_die_traumer@mail.ru>
2020-04-28 10:30:01 +0500

    Page object

---
+++ 4 section/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, en, fr or es")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    if user_language == "ru":
+        print("\nstart browser for ru-test..")
+        browser = webdriver.Chrome(options=options, executable_path='C:\chromedriver\chromedriver.exe')
+    elif user_language == "en":
+        print("\nstart browser for en-test..")
+        browser = webdriver.Chrome(options=options, executable_path='C:\chromedriver\chromedriver.exe')
+    else:
+        raise pytest.UsageError("--language should be ru or en")
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
