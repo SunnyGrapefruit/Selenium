962f72565c45cb85340ebc8a791ca74138072c5e
SunnyGrapefruit <wir_die_traumer@mail.ru>
2020-05-01 22:53:15 +0500

    Page object

--- 4 section/conftest.py
+++ 4 section/conftest.py
@@ -3,36 +3,36 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# def pytest_addoption(parser):
-#     parser.addoption('--language', action='store', default=None,
-#                      help="Choose language: ru, en, fr or es")
-
-
-# @pytest.fixture(scope="function")
-# def browser(request):
-#
-#     user_language = request.config.getoption("language")
-#     options = Options()
-#     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-#
-#     if user_language == "ru":
-#         print("\nstart browser for ru-test..")
-#         browser = webdriver.Chrome(options=options, executable_path='C:\chromedriver\chromedriver.exe')
-#     elif user_language == "en":
-#         print("\nstart browser for en-test..")
-#         browser = webdriver.Chrome(options=options, executable_path='C:\chromedriver\chromedriver.exe')
-#     else:
-#         raise pytest.UsageError("--language should be ru or en")
-#
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, en, fr or es")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
 
-    browser = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+    if user_language == "ru":
+        print("\nstart browser for ru-test..")
+        browser = webdriver.Chrome(options=options, executable_path='C:\chromedriver\chromedriver.exe')
+    elif user_language == "en":
+        print("\nstart browser for en-test..")
+        browser = webdriver.Chrome(options=options, executable_path='C:\chromedriver\chromedriver.exe')
+    else:
+        raise pytest.UsageError("--language should be ru or en")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+# @pytest.fixture(scope="function")
+# def browser(request):
+#
+#     browser = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
--- 4 section/test_product_page.py
+++ 4 section/test_product_page.py
@@ -6,14 +6,15 @@ from pages.login_page import LoginPage
 import pytest
 import faker
 
-# def test_guest_can_add_product_to_basket(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.add_to_cart()
-#     page.solve_quiz_and_get_code()
-#     page.assert_add_to_cart()
-#
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart()
+    # page.solve_quiz_and_get_code()
+    page.assert_add_to_cart()
+
 # def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 #     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 #     page = ProductPage(browser, link)
@@ -39,22 +40,23 @@ import faker
 #     page = ProductPage(browser, link)
 #     page.open()
 #     page.should_be_login_link()
-#
-# def test_guest_can_go_to_login_page_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.go_to_login_page()
 
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
-# def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-#     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-#     page = BasketPage(browser, link)
-#     page.open()
-#     # page.add_to_cart()
-#     page.go_to_cart()
-#     # page.no_item_in_cart()
-#     page.message_that_cart_is_empty()
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    # page.add_to_cart()
+    page.go_to_cart()
+    # page.no_item_in_cart()
+    page.message_that_cart_is_empty()
 
 # def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
@@ -64,18 +66,14 @@ import faker
 #     time.sleep(10)
 
 
-
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         link = "http://selenium1py.pythonanywhere.com/accounts/login/"
         self.page = LoginPage(browser, link)
         self.page.open()
-
         f = faker.Faker()       #используется спциальная билиотека Faker
-        email = f.email()
-
-
+        email = f.email()       #используется спциальная билиотека Faker
         self.page.register_new_user(email, email)
         self.page.should_be_authorized_user()
 
@@ -85,6 +83,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
